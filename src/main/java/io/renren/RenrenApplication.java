c5c405af55a5ade466373caeed025e532e3631a7
lipingyu <lipingyu@hdfax.com>
2019-11-29 17:21:58 +0800

    模板整理

--- src/main/java/io/renren/RenrenApplication.java
+++ src/main/java/io/renren/RenrenApplication.java
@@ -1,13 +1,7 @@
 package io.renren;
 
-import io.renren.datasource.DBIdentifier;
-import io.renren.entity.DataSourceInfo;
-import io.renren.utils.generator.GenUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
 
 
 @SpringBootApplication
