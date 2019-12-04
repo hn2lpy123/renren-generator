ded7ca9a56b4d9ad971edd4c5d03a1064741f49e
lipingyu <729172316@qq.com>
2019-12-05 00:36:54 +0800

    自定义导入模板

--- src/main/resources/static/js/extraFieldTable.js
+++ src/main/resources/static/js/extraFieldTable.js
@@ -6,7 +6,7 @@ function initExtraTable() {
         toggle: 'table',
         toolbar: "#toolbar",
         showFullscreen: true,
-        clickToSelect: false,
+        clickToSelect: true,
         search: false,
         showColumns: true,
         pageNumber : 1, //初始化加载第一页
@@ -20,17 +20,27 @@ function initExtraTable() {
                 "rows": res.data   //数据
             };
         },
+        formatLoadingMessage: function () {
+            return "请稍等，正在加载中...";
+        },
+        formatNoMatches: function () {  //没有匹配的结果
+            return '无符合条件的记录';
+        },
+        onLoadSuccess: function(){
+            $("[data-toggle='tooltip']").tooltip();
+        },
         showRefresh : true,//刷新按钮
+        showFooter: false,
         columns: [
+            { checkbox: true },
             { field: 'extraFieldType', title: 'ExtraFieldType', width: "150" },
             { field: 'extraFieldName', title: 'ExtraFieldValue', width: "300" },
-            { field: 'extraFieldValue', title: 'ExtraFieldValue', width: "900" },
+            { field: 'extraFieldValue', title: 'ExtraFieldValue', width: "600" },
             { field: 'operation', title: 'Operation', align: 'center',
                 formatter:function(value,row,index){
-                    console.log(row);
                     var element =
-                        "<button type='button' class='btn btn-warning btn-sm btn-xs' onclick='editExtraField(" + JSON.stringify(row) + ")'><i class=\"glyphicon glyphicon-edit\"></i> 编辑</button>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
-                        "<button type='button' class='btn btn-danger btn-sm btn-xs' onclick='deleteExtraField(" + JSON.stringify(row) + ")'><i class=\"glyphicon glyphicon-minus\"></i> 删除</button> ";
+                        "<button type='button' class='btn btn-warning btn-sm btn-xs' onclick='editExtraField(" + JSON.stringify(row) + ")'><i class=\"glyphicon glyphicon-edit\"></i> 编辑</button>"+
+                        "<button type='button' style='margin-left: 15px;' class='btn btn-danger btn-sm btn-xs' onclick='deleteExtraField(" + JSON.stringify(row) + ")'><i class=\"glyphicon glyphicon-minus\"></i> 删除</button> ";
                     return element;
                 }
             }
